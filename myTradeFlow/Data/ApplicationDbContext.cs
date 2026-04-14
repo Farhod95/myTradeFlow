705b6852f3a87dce7f3f988d0ed09df557ef91c3
Farhod <farhodbekisomiddinov5@gmail.com>
2026-04-14 14:06:12 +0500

    Added folder Data and class ApplicationDbContext

---
+++ myTradeFlow/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myTradeFlow.Models.Brands;
+using myTradeFlow.Models.Categories;
+
+namespace myTradeFlow.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        DbSet<Category> Categories { get; set; }
+        DbSet<Brand> Brands { get; set; }
+    }
+}
