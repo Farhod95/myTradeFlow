eb5b3dfabb797a6982eb8e5ae60e0415fa23c50f
Farhod <farhodbekisomiddinov5@gmail.com>
2026-04-14 10:54:56 +0500

    Added models

---
+++ myTradeFlow/Models/Brands/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace myTradeFlow.Models.Brands
+{
+    public class Brand
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Country { get; set; }
+        public DateTime CreateData { get; set; }
+        public DateTime UpdateData { get; set; } 
+    }
+}
---
+++ myTradeFlow/Models/Categories/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace myTradeFlow.Models.Categories
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateData { get; set; }
+        public DateTime UpdateData { get; set; }
+    }
+}
