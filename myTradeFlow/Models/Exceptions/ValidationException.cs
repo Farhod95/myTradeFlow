65f346a01c16b410e06282baf311caddd1da41b3
Farhod <farhodbekisomiddinov5@gmail.com>
2026-04-14 11:05:57 +0500

    Added model Exceptions

---
+++ myTradeFlow/Models/Exceptions/ValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace myTradeFlow.Models.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+        { }
+
+        public ValidationException(string? message) : base(message)
+        { }
+
+        public ValidationException(string? message, Exception? innerException) : base(message, innerException)
+        { }
+    }
+}
