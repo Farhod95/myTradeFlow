e67d112a96279740951456b4f3b0f0c8442f56ab
Farhod <farhodbekisomiddinov5@gmail.com>
2026-04-15 08:16:04 +0500

    Added foler Service

---
+++ myTradeFlow/Services/Brands/BrandService.cs
@@ -0,0 +1,13 @@
+﻿using myTradeFlow.Data;
+
+namespace myTradeFlow.Services.Brands
+{
+    public class BrandService : IBrandService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public BrandService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+    }
+}
