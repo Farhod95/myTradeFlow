c58978530268b98c42269ceecb75b1f2d6714d92
Farhod <farhodbekisomiddinov5@gmail.com>
2026-04-15 10:55:59 +0500

    Added folder Controller

--- myTradeFlow/Program.cs
+++ myTradeFlow/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using myTradeFlow.Data;
 using myTradeFlow.Repositories.Brands;
 using myTradeFlow.Repositories.Categories;
+using myTradeFlow.Services.Brands;
+using myTradeFlow.Services.Categories;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IBrandRepository, BrandRepository>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IBrandService, BrandService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
